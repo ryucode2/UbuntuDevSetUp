3c2bc7982722008192fa44fbb8069a824a15edb9
ryucode2 <error404-code@hotmail.com>
2025-09-17 06:02:40 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -15,7 +15,7 @@ APT_PACKAGES=(
 APT_OPTIONAL=(gnupg htop npm rsync fonts-firacode)
 
 sudo apt-get update -qq
-sudo apt-get install -y "${APT_PACKAGES[@]}" "${APT_OPTIONAL[@]}" dconf-cli gnome-tweaks gnome-extensions-app
+sudo apt-get install -y "${APT_PACKAGES[@]}" "${APT_OPTIONAL[@]}" dconf-cli gnome-tweaks gnome-extensions-app xclip
 
 # -----------------------------
 # Extra: Install Zsh plugins if missing
